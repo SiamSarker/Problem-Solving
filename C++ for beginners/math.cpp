f170e5eb684b49021aa48a637f45eb6e3e3ff33a
SiamSarker <sarkersiam2@gmail.com>
2020-10-17 19:56:34 +0600

    algorithm include added

---
+++ C++ for beginners/math.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+// for GCD
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    // left shift (by binary) means cross number (double)
+    // right shift means devied number
+
+    int a = 5 % 6;
+
+    cout << __algo_gcd(7, 12) << endl;
+
+    return 0;
+}
