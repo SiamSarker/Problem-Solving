4069e585d85c94abc3aa86c3b5d002ef0b8261d8
SiamSarker <sarkersiam2@gmail.com>
2020-10-17 19:44:58 +0600

    input1 added

---
+++ C++ for beginners/input1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    double b;
+    char c;
+    string d;
+
+    cin >> a >> b >> c >> d;
+    cout << a << " " << b << " " << c << " " << d << endl;
+    return 0;
+}
