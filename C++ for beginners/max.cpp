5d59d011d6e45b36c4b5b7a7842aec8f8372156e
SiamSarker <sarkersiam2@gmail.com>
2020-10-17 20:10:19 +0600

    max tried

---
+++ C++ for beginners/max.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int a, b, c; cin >> a >> b >> c;
+    cout << max({a,b,c}) << endl;
+    //cout << min({a,b,c}) << endl;
+    cout << "Hello World" << endl;
+    return 0;
+}
