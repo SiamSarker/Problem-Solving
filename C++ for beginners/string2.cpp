e3ecbc275d3d1a57faa8ab876fb823d13d608509
SiamSarker <sarkersiam2@gmail.com>
2020-10-17 21:02:12 +0600

    compare

---
+++ C++ for beginners/string2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a = "Siam";
+    string b = "Sarker";
+
+    if ( a < b)
+        cout << "Yes" << endl;
+    else
+        cout << "NO " << endl;
+
+
+    return 0;
+}
