92cab4fc58363d9b562aa6bca1c6f57d14c81670
SiamSarker <sarkersiam2@gmail.com>
2020-10-21 02:02:04 +0600

    file append added

---
+++ C++ for beginners/file4_append.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main()
+{
+    ifstream fin("file2.txt");
+    ofstream fout("fileoutput.txt", ofstream::app);
+    if (fin.is_open())
+    {
+        cout << "File exist" << endl;
+        int n; fin >> n;
+        vector <int> v(n);
+        //v.resize(n);
+        for (int i = 0; i < n; i++)
+        {
+            fin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        for(int i = 0; i < n; i++) fout << v[i] << " ";
+        fout << endl;       
+
+    }
+    else
+    {
+        cout << "File does not exist" << endl;
+    }
+    
+
+    return 0;
+}
