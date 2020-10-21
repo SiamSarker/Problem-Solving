ddc303d0313dccedbc6f6db91a58298ff8189a90
SiamSarker <sarkersiam2@gmail.com>
2020-10-21 13:05:26 +0600

    set 2 added

--- C++ for beginners/set2.cpp
+++ C++ for beginners/set2.cpp
@@ -12,14 +12,21 @@ using namespace std;
 int main()
 {
     set <int> st;
-    int n; cin >> n;
+    int n, x; cin >> n;
     for (auto i = 0; i<n; i++) 
     {
-        
+
         int temp ;
         cin >> temp;
         st.insert(temp);
     }
+
+    for(int x : st)
+        cout << x << " ";
+    cout << endl;
+
+    cin >> x;
+    st.erase(x);
 
     for(int x : st)
         cout << x << " ";
