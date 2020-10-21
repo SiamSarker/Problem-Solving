0b5f65fd1673588872b60dd5cfe9fd575dde4253
SiamSarker <sarkersiam2@gmail.com>
2020-10-21 23:19:44 +0600

    map1 added

---
+++ C++ for beginners/map1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <iterator>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include <list>
+#include <utility> 
+#include <map>
+
+using namespace std;
+
+
+int main()
+{
+    map <string, int> mp;
+    int n; cin >> n;
+    while(n--)
+    {
+        int x; cin >> x;
+        string ac_no; cin >> ac_no;
+        int amount;
+        if(x == 1)
+        {
+            cin >> amount;
+            mp[ac_no] += amount;
+        }
+        else cout << ac_no << " " << mp[ac_no] << endl;
+
+    }
+
+
+    return 0;
+}
