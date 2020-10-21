e17866208cf95363095af48c303b8c4e6c565934
SiamSarker <sarkersiam2@gmail.com>
2020-10-21 13:01:46 +0600

    set2 added

---
+++ C++ for beginners/set2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <iterator>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    set <int> st;
+    int n; cin >> n;
+    for (auto i = 0; i<n; i++) 
+    {
+        
+        int temp ;
+        cin >> temp;
+        st.insert(temp);
+    }
+
+    for(int x : st)
+        cout << x << " ";
+    cout << endl;
+    
+    return 0;
+}
