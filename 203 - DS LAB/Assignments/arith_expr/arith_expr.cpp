11b18d6e3ddf7037df1399f383f6b7cbf081cc96
SiamSarker <sarkersiam2@gmail.com>
2020-12-14 17:21:33 +0600

    Arithmetic Expression finally done

--- 203 - DS LAB/Assignments/arith_expr/arith_expr.cpp
+++ 203 - DS LAB/Assignments/arith_expr/arith_expr.cpp
@@ -92,77 +92,77 @@ int main()
         if ('0' <= token && token <= '9')
         {
             // write your code here
-            val_stack.push(token-'0');
+            val_stack.push(token-'0');      // pushing integer value to the value stack by subtracting '0' from the found character
         }
         else if (token == '(')
         {
             // write your code here
-            op_stack.push(token);
+            op_stack.push(token);       //  pushing character operation to the operation stack
         }
         else if (token == ')')
         {
             // write your code here
-            while (op_stack.peek() != '(')
+            while (op_stack.peek() != '(')       //   loop continues till found '(' char in the operation stack
             {
-                char op = op_stack.pop();
-                int a = val_stack.pop();
-                int b = val_stack.pop();
-                int result = applyOp(b, a, op);
-                val_stack.push(result);
+                char op = op_stack.pop();        //   poping a char from the operation stack
+                int a = val_stack.pop();        //   poping an integer from the value stack
+                int b = val_stack.pop();        //   poping another integer from the value stack
+                int result = applyOp(b, a, op);    //  b value added before a value in the stack, so sending b first would be correct oreder and it returns an integer result
+                val_stack.push(result);          // pushing the integer value to the value stack
             }
-            op_stack.pop();
+            op_stack.pop();         //   poping a char from operation stack to discard
         }
         else if (token == '+')
         {
             // write your code here
-            while (op_stack.is_empty() == false && op_stack.peek() != '(')
+            while (op_stack.is_empty() == false && op_stack.peek() != '(')      //   for checking whether the stack is not empty and loop continues till found '(' char in the operation stack
             {
-                char op = op_stack.pop();
-                int a = val_stack.pop();
-                int b = val_stack.pop();
-                int result = applyOp(b, a, op);
-                val_stack.push(result);
+                char op = op_stack.pop();       //   poping a char from the operation stack
+                int a = val_stack.pop();        //   poping an integer from the value stack
+                int b = val_stack.pop();        //   poping another integer from the value stack
+                int result = applyOp(b, a, op);     //  b value added before a value in the stack, so sending b first would be correct oreder and it returns an integer result
+                val_stack.push(result);     // pushing the integer value to the value stack
             }
-            op_stack.push('+');
+            op_stack.push('+');         //   pushing this operation char to the operation character
         }
         else if (token == '-')
         {
             // write your code here
-            while (op_stack.is_empty() == false && op_stack.peek() != '(')
+            while (op_stack.is_empty() == false && op_stack.peek() != '(')      //   for checking whether the stack is not empty and loop continues till found '(' char in the operation stack
             {
-                char op = op_stack.pop();
-                int a = val_stack.pop();
-                int b = val_stack.pop();
-                int result = applyOp(b, a, op);
-                val_stack.push(result);
+                char op = op_stack.pop();       //   poping a char from the operation stack
+                int a = val_stack.pop();        //   poping an integer from the value stack
+                int b = val_stack.pop();        //   poping another integer from the value stack
+                int result = applyOp(b, a, op);     //  b value added before a value in the stack, so sending b first would be correct oreder and it returns an integer result
+                val_stack.push(result);         // pushing the integer value to the value stack
             }
-            op_stack.push('-');
+            op_stack.push('-');         //   pushing this operation char to the operation character
         }
         else if (token == '*')
         {
             // write your code here
-            while (op_stack.is_empty() == false && op_stack.peek() == '*')
+            while (op_stack.is_empty() == false && op_stack.peek() == '*')      //   for checking whether the stack is not empty and loop continues if found '*' char in the operation stack
             {
-                char op = op_stack.pop();
-                int a = val_stack.pop();
-                int b = val_stack.pop();
-                int result = applyOp(b, a, op);
-                val_stack.push(result);
+                char op = op_stack.pop();       //   poping a char from the operation stack
+                int a = val_stack.pop();        //   poping an integer from the value stack
+                int b = val_stack.pop();        //   poping another integer from the value stack
+                int result = applyOp(b, a, op);     //  b value added before a value in the stack, so sending b first would be correct oreder and it returns an integer result
+                val_stack.push(result);     // pushing the integer value to the value stack
             }
-            op_stack.push('*');
+            op_stack.push('*');         //   pushing this operation char to the operation character
         }
         else if (token == '/')
         {
             // write your code here
-            while (op_stack.is_empty() == false && op_stack.peek() == '/')
+            while (op_stack.is_empty() == false && op_stack.peek() == '/')          //   for checking whether the stack is not empty and loop continues if found '/' char in the operation stack
             {
-                char op = op_stack.pop();
-                int a = val_stack.pop();
-                int b = val_stack.pop();
-                int result = applyOp(b, a, op);
-                val_stack.push(result);
+                char op = op_stack.pop();       //   poping a char from the operation stack
+                int a = val_stack.pop();        //   poping an integer from the value stack
+                int b = val_stack.pop();        //   poping another integer from the value stack
+                int result = applyOp(b, a, op);         //  b value added before a value in the stack, so sending b first would be correct oreder and it returns an integer result
+                val_stack.push(result);         // pushing the integer value to the value stack
             }
-            op_stack.push('/');
+            op_stack.push('/');         //   pushing this operation char to the operation character
         }
     }
 
@@ -170,15 +170,15 @@ int main()
     while (!op_stack.is_empty())
     {
         // write your code here
-        char op = op_stack.pop();
-        int a = val_stack.pop();
-        int b = val_stack.pop();
-        int result = applyOp(b, a, op);
-        val_stack.push(result);
+        char op = op_stack.pop();       //   poping a char from the operation stack
+        int a = val_stack.pop();        //   poping an integer from the value stack
+        int b = val_stack.pop();        //   poping another integer from the value stack
+        int result = applyOp(b, a, op);     //  b value added before a value in the stack, so sending b first would be correct oreder and it returns an integer result
+        val_stack.push(result);         // pushing the integer value to the value stack
     }
 
     // step 3:
-    printf("%d\n", val_stack.pop());
+    printf("%d\n", val_stack.pop());     //  printing the value from value stack
 
     return 0;
 }
