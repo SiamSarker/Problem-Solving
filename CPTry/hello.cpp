41f67b3f3ace8c361ff0b6632b98006cf69a7789
SiamSarker <sarkersiam2@gmail.com>
2021-02-28 04:47:31 +0600

    codeforce

--- CPTry/hello.cpp
+++ CPTry/hello.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	int i =0;
+	cin >> i;
     std::cout << "Hello World!";
     return 0;
 }
---
+++ CodeForces/4A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+    if(i % 2 == 0)
+    {
+        if ((i / 2) % 2 == 0)
+            cout << "YES";
+        else cout << "NO";
+    }
+    else
+        cout << "NO";
+}
