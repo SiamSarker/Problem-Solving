f19347838c573d77680fdd7847eaf562fc80c963
SiamSarker <sarkersiam2@gmail.com>
2021-03-02 00:11:20 +0600

    codeforce

---
+++ CodeForces/825A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    int a, b, count = 0;
+    cin >> a;
+    cin >> b;
+    string str = to_string(b);
+
+    int i = 0;
+    while (i != a)
+    {
+        if (str[i] == '0' && count == 0)
+            cout << '0' ;
+        else if (str[i] == '1')
+        {
+            while(str[i]!='0' && i != a)
+            {
+                count++;
+                i++;
+            }
+            cout << "hello";
+            if(count > 0)
+            {
+                // cout << count;
+                i++;
+            }
+            count = 0;
+        }
+        i++;
+    }
+
+    return 0;
+}
