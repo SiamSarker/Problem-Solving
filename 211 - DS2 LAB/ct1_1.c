5073fdc78751832f6ef544aaf492d0469204c3e1
SiamSarker <sarkersiam2@gmail.com>
2021-03-07 12:59:09 +0600

    class test

---
+++ 211 - DS2 LAB/ct1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int digit_sum(int num)
+{
+    if (num >= 0 && num <=7)
+        return num;
+    else
+        return digit_sum(num/10) + num%10 ;
+}
+
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    printf("%d\n", digit_sum(num));
+    return 0;
+}
