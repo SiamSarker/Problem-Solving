38f6918078273853d2da654790634054308f4abf
SiamSarker <sarkersiam2@gmail.com>
2021-03-17 02:34:26 +0600

    ok

---
+++ STL/cmake-build-debug/CMakeFiles/STL2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STL2.dir/comparator.cpp.o"
+  "STL2"
+  "STL2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STL2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
