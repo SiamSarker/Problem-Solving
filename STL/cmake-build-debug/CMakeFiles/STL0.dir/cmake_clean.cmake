d782f8775b65ece6aaccd124f75b80cfcf177dcf
SiamSarker <sarkersiam2@gmail.com>
2021-03-16 08:24:36 +0600

    clion

---
+++ STL/cmake-build-debug/CMakeFiles/STL0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STL0.dir/main.cpp.o"
+  "STL0"
+  "STL0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STL0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ STL/cmake-build-debug/CMakeFiles/STL1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STL1.dir/bitset_Sieve.cpp.o"
+  "STL1"
+  "STL1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STL1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
