8fbc4e58f5b163bb9f3f9c1650faf6a98ea61e35
SiamSarker <sarkersiam2@gmail.com>
2021-04-28 12:20:12 +0600

    git

--- 211 - DS2 LAB/DynamicProgramming/inclass.c
+++ 211 - DS2 LAB/DynamicProgramming/inclass.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 #define INF 99999
+int arr[1000][1000];
 
 int max(int a, int b) {
     if(a>b) return a;
     return b;
 }
-
-int LCS(char X[], int i, char Y[], int j, int dp[i][j]) {
+int LCS(char X[], int i, char Y[], int j) {
     if(i == 0 || j == 0)
-        return 0;
+        return arr[i][j];
 
-    if(dp[i-1][j-1] != INF) return dp[i - 1][j - 1];
-
+    if(arr[i-1][j-1] == INF){
     if(X[i] == Y[j])
-        return LCS(X, i-1, Y, j-1, dp) + 1;
+        arr[i-1][j-1] = LCS(X, i-1, Y, j-1) + 1;
     else {
-        int a = LCS(X, i, Y, j-1, dp);
-        int b = LCS(X, i-1, Y, j, dp);
-        return max(a, b);
+        int a = LCS(X, i, Y, j-1);
+        int b = LCS(X, i-1, Y, j);
+        arr[i-1][j-1] = max(a, b);
+        }
     }
+    return arr[i-1][j-1];
 }
 
 int main() {
-    char X[] = "~ABDE"; // we are not going to use index 0
-    char Y[] = "~ZBE"; // we are not going to use index 0
+ 
+    char X[] = "~ABDEASJDFLASJBDFLJASIUFASIDUF"; // we are not going to use index 0
+    char Y[] = "~ZBEFSIDHFAPDOIFHAOSF"; // we are not going to use index 0
     int m = sizeof(X)/sizeof(char)-2; // excluding '~' and '\0'
     int n = sizeof(Y)/sizeof(char)-2; // excluding '~' and '\0'
-    
-    int dp[m][n];
 
-    for(int i = 0 ; i <m; i++){
-        for(int j = 0; j<n; j++)
-            dp[i][j] = INF;
-       } 
+    for(int i = 0; i<= m;i++)
+        arr[0][i] = 0;
+    for(int i = 0; i<= n;i++)
+        arr[i][0] = 0;
 
-    printf("%d\n", LCS(X, m, Y, n, dp));
+    for(int i = 1 ; i <= m; i++)
+        for(int j = 1; j<n; j++)
+            arr[i][j] = INF;
+
+    printf("%d\n", LCS(X, m, Y, n));
     return 0;
 }
