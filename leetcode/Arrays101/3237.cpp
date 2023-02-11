2655dffebd08305f42ae36dadd439b89f0e26485
siamsarker <sarkersiam2@gmail.com>
2023-02-11 23:15:19 +0600

    3237

--- leetcode/Arrays101/3237.cpp
+++ leetcode/Arrays101/3237.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
-int main(){
-    cout << "Hello, world!" << endl;
+int main()
+{
+    int nums[] = {555,901,482,1771};
+    int even_digit_count = 0;
+
+    for (int num : nums)
+    {
+        int count = 0;
+        while (num)
+        {
+            num /= 10;
+            count++;
+        }
+        if (count % 2 == 0)
+            even_digit_count++;
+    }
+    cout << even_digit_count << endl;
+
     return 0;
 }
