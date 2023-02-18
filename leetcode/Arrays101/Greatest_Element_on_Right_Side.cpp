87750f45e0e4e48de5b4af713e61e435f37b6adf
siamsarker <sarkersiam2@gmail.com>
2023-02-18 19:38:43 +0600

    greatest element on right side

--- leetcode/Arrays101/Greatest_Element_on_Right_Side.cpp
+++ leetcode/Arrays101/Greatest_Element_on_Right_Side.cpp
@@ -9,24 +9,17 @@ int main()
 {
     vector<int> arr = {17, 18, 5, 4, 6, 1};
 
-    int m = 0;
+    int m = arr[arr.size() - 1];
+    int n = 0;
     
-    for (int i = 0; i < arr.size()-1; i++)
+    for (int i = arr.size()-1; i > 0; i--)
     {
-        // m = arr[i+1];
-        // for (int j = i+1; j < arr.size(); j++)
-        // {
-        //     if (arr[j] >= m) {
-        //         m = arr[j];
-        //     } 
-        // }
-        // arr[i] = m;
-        arr[i] = *max_element(arr.begin()+i+1, arr.end());
+        n = m;
+        if (arr[i-1] >= m) m = arr[i-1];
+        arr[i-1] = n;
     }
 
     arr[arr.size()-1]= -1;
-
-    
 
     for (int i = 0; i < arr.size(); i++)
         cout << arr[i] << " ";
