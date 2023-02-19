a4eca4c03f073dca1508b1e112b76bb56ef614ac
siamsarker <sarkersiam2@gmail.com>
2023-02-19 20:38:03 +0600

    third maximum number

--- leetcode/Arrays101/third_maximum_number.cpp
+++ leetcode/Arrays101/third_maximum_number.cpp
@@ -9,7 +9,8 @@ int main()
 {
     vector<int> nums = {2,2,3,1};
     
-    sort(nums.begin(), nums.end(), greater<int>());
+    sort(nums.begin(), nums.end(), greater <>());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
 
     for (int i = 0; i < nums.size(); i++)
         cout << nums[i] << " ";
